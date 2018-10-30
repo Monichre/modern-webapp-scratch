7e1a3dd842ad74730f97272c5e3097f821fa1a86
Liam Ellis <liamhellis@gmail.com>
2018-10-30 11:36:30 -0400

    basic config setup

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+// import { getBacon } from './utils'
+// const baconEl = document.querySelector('.bacon');
+// getBacon()
+//   .then(res => {
+//     const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
+//     baconEl.innerHTML = markup;
+//   }).catch(err => (baconEl.innerHTML = err));
---
+++ src/scripts/utils.js
@@ -0,0 +1,4 @@
+export const getBacon = () => {
+  const body = fetch('https://baconipsum.com/api/?type=all-meat&paras=3').then(res => res.json())
+  return body
+}
